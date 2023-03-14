d253f731f8a768fec02a25bf9326978850156ed9
unknown <gabrieltorres9909@gmail.com>
2023-03-14 09:22:04 -0600

    Dejando el index solo como archivo inicializador y agregando un app.js para todos los procesos de la app

--- server/index.js
+++ server/index.js
@@ -1,12 +1,7 @@
-import express from 'express'
-import postsRoutes from './routes/posts.routes.js';
 import {connectDB} from './db.js'
 import {PORT} from './config.js'
+import app from './app.js'
 
-
-
-const app = express();
-app.use(postsRoutes)
 app.listen(PORT)
 connectDB()
 

2b9bd1c2acc53c8ae7147aec552df437ff1d48b8
unknown <gabrieltorres9909@gmail.com>
2023-03-14 09:39:43 -0600

    Modiificando los request y agregando el uso de jsno

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
-import express from 'express'
+import express, { json } from 'express'
 import postsRoutes from './routes/posts.routes.js';
 
 const app = express();
+
+// Middlewares 
+app.use(express.json()) // Para poder utilizar json en los request a la base de datos 
 app.use(postsRoutes)
 
 export default app
--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,5 +1,13 @@
-export const getPosts = (req,res)=> res.send('Todos post')
-export const createPost = (req,res)=> res.send('Nuevo post')
+import Post from "../models/Posts.js"
+//Función que regresa todos los datos de Posts
+export const getPosts = async (req,res)=> {
+    const posts = await Post.find() 
+    res.send('Todos post')
+}
+export const createPost = (req,res)=> {
+    console.log(req.body)
+    res.send('Nuevo post')
+}
 export const updatePost = (req,res)=> res.send('Modificar post')
 export const removePost = (req,res)=> res.send('Eliminar post')
 export const getPost = (req,res)=> res.send('Mostrar post')

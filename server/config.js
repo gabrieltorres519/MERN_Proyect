7a967d3b2a80089b06f2c83fcfbf72d1915cc48d
unknown <gabrieltorres9909@gmail.com>
2023-03-14 09:00:12 -0600

    Agregando y exportando la variable de puerto y definiendo que se use un puerto u otro

--- server/config.js
+++ server/config.js
@@ -2,4 +2,5 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const MONGOOD_URI = process.env.MONGOOD_URI
+export const MONGOOD_URI = process.env.MONGOOD_URI || 'mongodb://localhost/baseMERN'
+export const PORT = process.env.PORT
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import postsRoutes from './routes/posts.routes.js';
 import {connectDB} from './db.js'
+import {PORT} from './config.js'
+
 
 
 const app = express();
 app.use(postsRoutes)
-app.listen(3000)
+app.listen(PORT)
 connectDB()
 
-console.log('Servidor corriendo en puerto',3000)
+console.log('Servidor corriendo en puerto', PORT)

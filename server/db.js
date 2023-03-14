67193681df0a7de49e4f8ffc1f78e8af9f00375e
unknown <gabrieltorres9909@gmail.com>
2023-03-14 08:46:21 -0600

    Usando el archivo .env y una constante para la cadena de conexión

--- server/db.js
+++ server/db.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
 export async function connectDB(){
     try {
-        const db = await mongoose.connect('mongodb://localhost/baseMERN')
-        console.log('Base de datos conectada a ', db)
+        const db = await mongoose.connect(process.env.MONGOOD_URI)
+        console.log('Base de datos conectada a ', db.connect.name)
     } catch (error) {
         console.log(error)
     }

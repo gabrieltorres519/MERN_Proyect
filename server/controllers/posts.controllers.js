f910e471df37b32157a0b151b8dc3492415ad1df
unknown <gabrieltorres9909@gmail.com>
2023-03-17 08:56:20 -0600

    Generando los controladores para mostrar e insertar datos

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -2,13 +2,25 @@ import Post from "../models/Posts.js"
 //Función que regresa todos los datos de Posts
 export const getPosts = async (req,res)=> {
     const posts = await Post.find() 
-    res.send('Todos post')
+    res.send(posts)
 }
-export const createPost = (req,res)=> {
+// Función parainsertar un dato
+export const createPost = async (req,res)=> {
+    const {title, description} = req.body
     console.log(req.body)
-    res.send('Nuevo post')
+    const newPost = new Post({title,description})
+    await newPost.save()
+    console.log(newPost)
+    return res.json(newPost)
 }
-export const updatePost = (req,res)=> res.send('Modificar post')
+// Función modificar dato
+export const updatePost = async (req,res)=> {
+    console.log(req.params)
+    console.log(req.body)
+    await Post.findByIdAndUpdate(req.params.id,body)
+    return res.send('Post Modificado')
+}
+
 export const removePost = (req,res)=> res.send('Eliminar post')
 export const getPost = (req,res)=> res.send('Mostrar post')
 
--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -11,11 +11,11 @@ const router = Router();
 
 router.get("/posts", getPosts);
 
-router.get("/posts/:id", getPost);
+router.get("/posts:id", getPost);
 
 router.post("/posts", createPost);
 
-router.put("/posts/:id", updatePost);
+router.put("/posts/id", updatePost);
 
 router.delete("/posts/:id", removePost);
 

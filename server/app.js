e407aac1aedd36a53f1900785640cc3d0bf68306
unknown <gabrieltorres9909@gmail.com>
2023-03-21 09:02:05 -0600

    importando los paquetes que se ocupen en app.js y usándolo con app.use() que es express

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 import express, { json } from 'express'
 import postsRoutes from './routes/posts.routes.js';
+import fileUpload from 'express-fileupload';
+// import { fstat } from 'fs-extra';
 
 const app = express();
 
 // Middlewares 
 app.use(express.json()) // Para poder utilizar json en los request a la base de datos 
+app.use(fileUpload())
+
+//Rutas
 app.use(postsRoutes)
 
 export default app

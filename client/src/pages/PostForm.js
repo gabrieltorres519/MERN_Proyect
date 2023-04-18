9767a381ead62ccbcfff9cf3a5701acb2c74147f
unknown <gabrieltorres9909@gmail.com>
2023-04-17 22:06:28 -0600

    Creado un formulario simple

--- client/src/pages/PostForm.js
+++ client/src/pages/PostForm.js
@@ -1,6 +1,15 @@
+import {Formik, Form, Field} from 'formik'
+
 export function PostForm(){
     return (
-        <div>Post form</div>
+        <div>
+            <Formik>
+                <Form>
+                    <Field placeholder="title"/>
+                    <Field placeholder="title"/>
+                </Form>
+            </Formik>
+        </div>
     )
 }
 

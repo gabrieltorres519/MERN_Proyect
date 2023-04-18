3f4c4aea7b8f9b450971295af5c21f84978290f2
unknown <gabrieltorres9909@gmail.com>
2023-04-17 23:29:10 -0600

    Mejorando la estética del formulario

--- client/src/pages/PostForm.js
+++ client/src/pages/PostForm.js
@@ -26,10 +26,14 @@ export function PostForm(){
             
             {({handleSubmit}) => (
                 <Form onSubmit={handleSubmit}>
-                    <Field name='title' placeholder="title"/>
-                    <ErrorMessage name='title'/>
-                    <Field name='description' placeholder="description"/>
-                    <ErrorMessage name='description'/>
+                    <Field name='title' placeholder="title"
+                        className='px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full'
+                    />
+                    <ErrorMessage component="p" className='text-red-400 text-sm' name='title'/>
+                    <Field name='description' placeholder="description"
+                        className='px-3 py-2 focus:outline-none rounded bg-gray-600 text-white w-full'
+                    />
+                    <ErrorMessage component="p" className='text-red-400 text-sm' name='description'/>
 
                     <button type='submit'>Save</button>
                 </Form>
